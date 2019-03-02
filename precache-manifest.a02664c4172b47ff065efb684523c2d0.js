bb09b3eaf14b4b54f2540068c2566fcda8a4c2a0
Brian Spit <brian@mythenatic.com>
2019-03-02 04:44:06 +0100

    Updates

---
+++ precache-manifest.a02664c4172b47ff065efb684523c2d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5aa12ca440d7a4cfe4fe",
+    "url": "/obs-voice-control/static/js/runtime~main.5aa12ca4.js"
+  },
+  {
+    "revision": "a42fb150c2b74f1291b8",
+    "url": "/obs-voice-control/static/js/main.a42fb150.chunk.js"
+  },
+  {
+    "revision": "d645295da177f3e74e48",
+    "url": "/obs-voice-control/static/js/2.d645295d.chunk.js"
+  },
+  {
+    "revision": "a42fb150c2b74f1291b8",
+    "url": "/obs-voice-control/static/css/main.3a761f31.chunk.css"
+  },
+  {
+    "revision": "b2a693f282aa69b3df09e85431eb8c55",
+    "url": "/obs-voice-control/index.html"
+  }
+];
